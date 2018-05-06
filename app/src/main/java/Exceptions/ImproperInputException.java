68b892c04fea921fd0c0205efc9d807567cdb0da
rjarsenault1101 <rjarsenault1101@gmail.com>
2018-05-05 21:22:41 -0300

    initial commit

---
+++ app/src/main/java/Exceptions/ImproperInputException.java
@@ -0,0 +1,11 @@
+package Exceptions;
+
+/**
+ * Created by The Smarticus 2017 on 2018-05-05.
+ */
+
+public class ImproperInputException extends Exception {
+    public ImproperInputException(String s) {
+        super(s);
+    }
+}
---
+++ app/src/test/java/arsenault/rj/homelandsecurity/ExampleUnitTest.java
@@ -0,0 +1,16 @@
+package arsenault.rj.homelandsecurity;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ExampleUnitTest {
+    @Test
+    public void MapBuilding() throws Exception {
+        String input = "(1,2,3),(4,5,6),(7,8,9)";
+        final String expectedOutput = "(1,2,3),(4,5,6),(7,8,9)";
+        int[][][] arr = MainActivity.makeMap(input);
+        String output = MainActivity.printArray(arr);
+        assertEquals(expectedOutput, output);
+    }
+}
